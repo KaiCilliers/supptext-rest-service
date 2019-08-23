1a9564248f805f5b0b8e5afced95811892a31a0b
Kai Cilliers <kaijohan428@gmail.com>
2019-08-23 17:01:29 +0200

    message model updated

--- models/message.js
+++ models/message.js
@@ -10,15 +10,6 @@ const mongoose = require('mongoose');
 const messageSchema = new mongoose.Schema({
     room: {
         type: new mongoose.Schema({
-            created_at: {
-                type: Date,
-                required: true,
-                default: Date.now
-            },
-            private: {
-                type: Boolean,
-                required: true
-            },
             last_message: {
                 type: Date
             }
@@ -33,24 +24,12 @@ const messageSchema = new mongoose.Schema({
                 minlength: 2,
                 maxlength: 50
             },
-            last_name: {
-                type: String,
-                required: true,
-                minlength: 3,
-                maxlength: 50
-            },
             phone: {
                 type: String,
                 unique: true,
                 required: true,
                 minlength: 10,
                 maxlength: 15
-            },
-            status: {
-                type: String,
-                minlength: 1,
-                maxlength: 255,
-                default: 'Supp! Want to chat?'
             }
         }),
         required: true
@@ -73,12 +52,6 @@ const messageSchema = new mongoose.Schema({
         default: false
     }
 });
-messageSchema.statics.lookup = function(userId, roomId) {
-    return this.findOne({
-        'user._id': userId,
-        'room._id': roomId
-    });
-}
 const Message = mongoose.model('Message', messageSchema);
 
 /**
@@ -88,11 +61,9 @@ const Message = mongoose.model('Message', messageSchema);
  */
 function validateMessage(message) {
     const schema = {
-        userId: Joi.objectId().required(),
-        roomId: Joi.objectId().required(),
         content: Joi.string().min(1).max(255).required()
     }
-    return Joi.validate(rental, schema);
+    return Joi.validate(message, schema);
 }
 
 /**
